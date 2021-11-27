346b56467c40cb288154f8e9ce17e80eaca2c908
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-11-27 22:47:05 +0900

    first commit

---
+++ src/components/Book.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  setup() {
+    console.log("book");
+  },
+});
+</script>
+
+<template>
+  <div class="book">book</div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <p class="main__header">Hello World</p>
+    <Book />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import Book from "./Book.vue";
+
+export default defineComponent({
+  components: {
+    Book: Book,
+  },
+  setup() {
+    console.log("hello world");
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.main {
+  &__header {
+    color: red;
+  }
+}
+</style>
