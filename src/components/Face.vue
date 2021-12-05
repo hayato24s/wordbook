8d093386c6b4be5d225f665c18447a9e4107b862
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-12-05 17:59:26 +0900

    add: icon button

---
+++ src/components/Face.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+import { defineComponent, PropType, computed } from "@vue/runtime-core";
+import { Evaluation } from "~/entities/evaluation";
+
+const iconNameMap: Record<Evaluation, string> = {
+  Excellent: "sentiment_very_satisfied",
+  Good: "sentiment_satisfied",
+  Poor: "sentiment_dissatisfied",
+  NotLearned: "sentiment_neutral",
+};
+
+export default defineComponent({
+  name: "Face",
+  props: {
+    name: {
+      type: String as PropType<Evaluation>,
+      required: true,
+    },
+  },
+  emits: ["click"],
+  setup(props) {
+    const color = computed(() => {
+      return props.name === "NotLearned"
+        ? "not-learned"
+        : props.name.toLowerCase();
+    });
+    return {
+      color,
+      iconNameMap,
+    };
+  },
+});
+</script>
+
+<template>
+  <div @click="$emit('click')" :class="`face material-icons --${color}`">
+    {{ iconNameMap[name] }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.face {
+  display: inline-block;
+  width: min-content;
+
+  @include button-cursor;
+
+  &.--excellent {
+    color: $excellent;
+  }
+
+  &.--good {
+    color: $good;
+  }
+
+  &.--poor {
+    color: $poor;
+  }
+
+  &.--not-learned {
+    color: $not-learned;
+  }
+}
+</style>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,83 @@
+<script lang="ts">
+import { defineComponent, PropType } from "@vue/runtime-core";
+
+const iconNameMap = {
+  back: "keyboard_backspace",
+  reverse: "swap_vert",
+  shuffle: "shuffle",
+  list: "format_list_bulleted",
+  play: "play_circle_outline",
+  next: "skip_next",
+  prev: "skip_previous",
+  listening: "headphones",
+  learn: "description",
+  right: "chevron_right",
+};
+
+type IconName = keyof typeof iconNameMap;
+
+export default defineComponent({
+  name: "IconButton",
+  props: {
+    size: {
+      type: String as PropType<"small" | "large">,
+      default: "small",
+    },
+    color: {
+      type: String as PropType<"black" | "red" | "gray">,
+      default: "black",
+    },
+    iconName: {
+      type: String as PropType<IconName>,
+      required: true,
+    },
+  },
+  emits: ["click"],
+  setup() {
+    return {
+      iconNameMap,
+    };
+  },
+});
+</script>
+
+<template>
+  <div @click="$emit('click')" class="icon-button">
+    <span :class="`icon-button__icon material-icons --${size} --${color}`">{{
+      iconNameMap[iconName]
+    }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.icon-button {
+  display: inline-block;
+  width: min-content;
+
+  @include button-cursor;
+
+  &__icon {
+    &.--small {
+      font-size: 2.4rem;
+    }
+
+    &.--large {
+      font-size: 4.8rem;
+    }
+
+    &.--black {
+      color: $black;
+    }
+
+    &.--red {
+      color: $bright-red;
+    }
+
+    &.--gray {
+      color: $gray;
+    }
+  }
+}
+</style>
