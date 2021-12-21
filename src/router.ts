ed62994202462ee1e6a06e61fcde843780c5769f
Hayato Shishido <hayato24sh@gmail.com>
2021-12-21 18:23:27 +0900

    remove: unnecessary pages

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "~/pages/index.vue";
+import Login from "~/pages/login.vue";
 import Learn from "~/pages/learn/index.vue";
 import Problem from "~/pages/learn/problem.vue";
-import Detail from "~/pages/listen/detail.vue";
 import Filter from "~/pages/listen/filter.vue";
 import Listen from "~/pages/listen/index.vue";
-import Preview from "~/pages/Preview.vue";
 
 const history = createWebHistory();
 
 const routes = [
-  { path: "/", component: Preview },
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
   { path: "/learn", component: Learn },
   { path: "/learn/problem", component: Problem },
   { path: "/listen", component: Listen },
-  { path: "/listen/detail", component: Detail },
   { path: "/listen/filter", component: Filter },
-  { path: "/preview", component: Preview },
 ];
 
 export const router = createRouter({
