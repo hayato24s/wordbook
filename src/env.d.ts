37f2475ce09b26413bc8873c5b0663a4fd220f25
Hayato Shishido <hayato24sh@gmail.com>
2021-12-23 00:37:09 +0900

    add: env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_AUTH_DOMAIN: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_STORAGE_BUCKET: string;
+  readonly VITE_MESSAGING_SENDERID: string;
+  readonly VITE_APP_ID: string;
+  readonly VITE_MEASUREMENT_ID: string;
+}
+
 declare module "*.vue" {
   import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
