1e638ce4e0d9e4bc154613b9044c964e5f08140a
Hayato Shishido <hayato24sh@gmail.com>
2021-12-23 16:51:35 +0900

    prepare hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path/posix";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "src/main.ts"),
+      },
+    },
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
